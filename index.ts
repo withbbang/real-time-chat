c8abb1a0bbaf4edf71469d8b292024f9512bb4da
Bread <withbbang@gmail.com>
2022-01-14 23:34:09 +0900

    typescript nodemon 적용하느라 개고생함 ㅅㅂ

--- index.ts
+++ index.ts
@@ -1,5 +1,16 @@
+import express, { Request, Response } from "express";
+import http from "http";
 import { str } from "./module";
+const app = express();
+const server = http.createServer(app);
+const PORT: Number = 3000;
+// http server를 socket.io server로 upgrade한다
+const io = require("socket.io")(server);
 
-const message: String = "hello world";
-console.log(message);
-console.log(str);
+app.get("/", (req: Request, res: Response) => {
+  res.send(str);
+});
+
+server.listen(PORT || 3000, function () {
+  console.log(`Socket IO server listening on ${PORT}`);
+});
